07282aadb3b08b7901fee54b411da7d348cb50a4
Sasha Donets <sashadoneze@gmail.com>
2022-07-14 21:36:37 +0300

    Connect Skills component to the App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
