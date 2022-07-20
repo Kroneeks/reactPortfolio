71988022b77316988c515cefa2c62f39cf7ff4a8
Sasha Donets <sashadoneze@gmail.com>
2022-07-20 22:57:50 +0300

    Add 2-step code for nodemailer

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
+
+dotenv.config();
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
@@ -15,7 +18,7 @@ const contactEmail = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "bobrmeat@gmail.com",
-    pass: "sasha14032001",
+    pass: "umwdtvdprmmvovlo",
   },
 });
 
